53557887e330f23a46f3d4d4c3291589839e4e45
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-11-01 11:56:26 +0000

    Task 3: Parametrize templates Use the _ or gettext function to parametrize your templates. Use the message IDs home_title and home_header.
    Create a babel.cfg file containing
    
    [python: **.py]
    [jinja2: **/templates/**.html]
    extensions=jinja2.ext.autoescape,jinja2.ext.with_
    Then initialize your translations with
    
    $ pybabel extract -F babel.cfg -o messages.pot .
    and your two dictionaries with
    
    $ pybabel init -i messages.pot -d translations -l en
    $ pybabel init -i messages.pot -d translations -l fr
    Then edit files translations/[en|fr]/LC_MESSAGES/messages.po to provide the correct value for each message ID for each language. Use the following translations:
    
    msgidEnglishFrench
    home_title"Welcome to Holberton""Bienvenue chez Holberton"
    home_header"Hello world!""Bonjour monde!"
    Then compile your dictionaries with
    
    $ pybabel compile -d translations
    Reload the home page of your app and make sure that the correct messages show up.

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,6 +21,7 @@ class Config(object):
 # configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
@@ -38,7 +39,7 @@ def get_locale():
 def index():
     """_summary_
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
